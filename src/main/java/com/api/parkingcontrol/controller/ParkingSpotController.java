bf4f7ece9f4e225a21fe21507cef15a1ec801456
jaque20050 <jaque20050@gmail.com>
2022-02-11 17:16:08 -0300

    Gerando Dto

---
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parkingcontrol.service.ParkingSpotService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+	@Autowired
+	ParkingSpotService parkingSpotService;
+
+	
+}
