e13eed3072ac1335fa70a3f9e382eae6d98279b5
jaque20050 <jaque20050@gmail.com>
2022-02-15 14:29:29 -0300

    Implementação do método PUT

--- src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controller/ParkingSpotController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,4 +79,19 @@ public class ParkingSpotController {
 		parkingSpotService.delete(parkingSpotModelOptional.get());
 		return ResponseEntity.status(HttpStatus.OK).body("Vaga de estacionamento encontrada, deletada com sucesso!!! ");
 	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id,
+			@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+		Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+		if (!parkingSpotModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Essa vaga de estacionamento, não foi encontrada.");
+		}
+		var parkingSpotModel = new ParkingSpotModel();
+		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+		parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+		parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+	}
+
 }
